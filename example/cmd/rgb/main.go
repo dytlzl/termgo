5382ed63e6a4409f9baddd3a4e9d302d681042a8
dytlzl <dytlzl.dev@gmail.com>
2022-06-26 03:12:50 +0900

    Fix

--- example/cmd/rgb/main.go
+++ example/cmd/rgb/main.go
@@ -12,22 +12,18 @@ func main() {
 	}
 }
 
-var style = tui.Style{F256: 218, B256: 53}
-
 func rootView() *tui.View {
 	values := []int{0, 95, 135, 175, 215, 255}
-	return tui.VStack(
-		tui.VMap(values, func(r int) *tui.View {
-			return tui.VMap(values, func(g int) *tui.View {
-				return tui.HMap(values, func(b int) *tui.View {
-					return tui.P(
-						tui.Span(" "),
-						tui.Fmt("%3d, %3d, %3d", r, g, b).
-							FGColor(tui.If(color.RelativeBrightness(r, g, b) > 0.5, color.RGB(0, 0, 0), color.RGB(255, 255, 255))).
-							BGColor(color.RGB(r, g, b)),
-					)
-				}).AbsoluteSize(0, 1)
-			})
-		}),
-	).Border().Title("RGB").AbsoluteSize(89, 40)
+	return tui.VMap(values, func(r int) *tui.View {
+		return tui.VMap(values, func(g int) *tui.View {
+			return tui.HMap(values, func(b int) *tui.View {
+				return tui.P(
+					tui.Span(" "),
+					tui.Fmt("%3d, %3d, %3d", r, g, b).
+						FGColor(tui.If(color.RelativeBrightness(r, g, b) > 0.5, color.RGB(0, 0, 0), color.RGB(255, 255, 255))).
+						BGColor(color.RGB(r, g, b)),
+				)
+			}).AbsoluteSize(0, 1)
+		})
+	}).Border().Title("RGB").AbsoluteSize(89, 40)
 }
