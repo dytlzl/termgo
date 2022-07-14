73bda1c41b5cb0623f2f77d4847efc93327f1703
dytlzl <dytlzl.dev@gmail.com>
2022-07-14 15:09:41 +0900

    Handle ^[OA

--- pkg/tui/buffer.go
+++ pkg/tui/buffer.go
@@ -9,8 +9,12 @@ func readBuffer(buffer []rune) (rune, int) {
 	if len(buffer) > 2 {
 		v := buffer[0]*0x10000 + buffer[1]*0x100 + buffer[2]
 		switch v {
+		// ^[[A
 		case key.ArrowUp, key.ArrowDown, key.ArrowRight, key.ArrowLeft:
 			return v, 3
+		// ^[OA
+		case key.ArrowUp - 0x1200, key.ArrowDown - 0x1200, key.ArrowRight - 0x1200, key.ArrowLeft - 0x1200:
+			return v + 0x1200, 3
 		}
 	}
 	return buffer[0], 1
